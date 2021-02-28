4ff0f8e6df7dc037596c2785c2fe0790da4d8eb2
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-02-28 16:16:01 -0600

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -5,6 +5,5 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     # This is called for every test. Only get/set command line arguments
     # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.token
-    if 'token' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("token", [option_value])
+    if 'token' in metafunc.fixturenames:
+        metafunc.parametrize("token", metafunc.config.getoption('token'))
