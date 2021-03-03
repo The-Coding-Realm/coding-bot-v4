40e6decaef124cbc8a1c1fbf86359c366146375a
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-03-02 23:21:25 -0600

    Update mod.py

--- cogs/mod.py
+++ cogs/mod.py
@@ -6,6 +6,33 @@ class Moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         
+    async def log(self, **kwargs):
+        action = kwargs.pop(type)
+        action_string = action + 'ed' if not action.endswith('e') else action + 'd'
+        moderator = kwargs.pop(moderator)
+        target = kwargs.pop(target)
+        reason = kwargs.get('reason')
+        if action = 'kick':
+            color = discord.Color.yellow()
+        elif action = 'ban':
+            color = discord.Color.red()
+        elif action = 'warn':
+            color = discord.Color.green()
+        elif action = 'mute':
+            color = discord.Color.blue()
+        else:
+            raise ValueError('Incorrect Type')
+        embed = discord.Embed(title=f'Member {action_string}'
+                              color=color)
+        embed.add_field(name='Moderator',
+                        description=moderator.mention)
+        embed.add_field(name='Target',
+                        description=target.mention)
+        embed.add_field(name='Reason',
+                        description=reason)
+        logs = bot.get_channel(757433319569883146)
+        await logs.send(embed=embed)
+        
     @commands.command(name='kick')
     @commands.bot_has_guild_permissions(kick_members=True)
     @commands.has_guild_permissions(kick_members=True)
@@ -16,6 +43,7 @@ class Moderation(commands.Cog):
             await target.send(f'You have been kicked from **{ctx.guild.name}**. \nReason: {reason}')
         except:
             pass
+        await self.log(action='kick', moderator=ctx.author, target=target, reason=reason)
         await target.kick(reason=f'{ctx.author.id}: {reason}')
         await ctx.send(embed=discord.Embed(title='Member kicked',
                                            description=f'{target.mention} has been kicked \nReason: {reason}'))
