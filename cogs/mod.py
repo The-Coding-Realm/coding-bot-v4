d22525c0bb3208f7ec9fec28607df394d53b8dd9
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-03-03 11:48:58 -0600

    add a warn command

--- cogs/mod.py
+++ cogs/mod.py
@@ -38,35 +38,53 @@ class Moderation(commands.Cog):
         logs = self.bot.get_channel(757433319569883146)
         await logs.send(embed=embed)
         
+    @commands.check()
+    async def moderation_check(self):
+        if ctx.author.top_role <= target.top_role:
+            await ctx.send(embed=ctx.error('You cannot use moderation commands on users higher or equal to you.'))
+            return False
+        return True
+        
     @commands.command(name='kick')
     @commands.bot_has_guild_permissions(kick_members=True)
     @commands.has_guild_permissions(kick_members=True)
+    @moderation_check
     async def _kick(self, ctx, target: discord.Member, *, reason: str = None):
-        if ctx.author.top_role <= target.top_role:
-            return await ctx.send(embed=ctx.error('You cannot use moderation commands on users higher or equal to you.'))
         try:
-            await target.send(f'You have been kicked from **{ctx.guild.name}**. \nReason: {reason}')
+            await target.send(f'You have been :boot: kicked :boot: from **{ctx.guild.name}**. \nReason: {reason}')
         except:
             pass
         await self.log(action='kick', moderator=ctx.author, target=target, reason=reason)
         await target.kick(reason=f'{ctx.author.id}: {reason}')
-        await ctx.send(embed=discord.Embed(title='Member Kicked',
+        await ctx.send(embed=discord.Embed(title=':boot: Member Kicked :boot:',
                                            description=f'{target.mention} has been kicked \nReason: {reason}'))
         
     @commands.command(name='ban')
     @commands.bot_has_guild_permissions(ban_members=True)
     @commands.has_guild_permissions(ban_members=True)
+    @moderation_check
     async def _ban(self, ctx, target: discord.Member, *, reason: str = None):
-        if ctx.author.top_role <= target.top_role:
-            return await ctx.send(embed=ctx.error('You cannot use moderation commands on users higher or equal to you.'))
-        try:
-            await target.send(f'You have been banned from **{ctx.guild.name}**. \nReason: {reason}')
+            await target.send(f'You have been :no_entry: banned :no_entry: from **{ctx.guild.name}**. \nReason: {reason}')
         except:
             pass
         await self.log(action='ban', moderator=ctx.author, target=target, reason=reason)
         await target.kick(reason=f'{ctx.author.id}: {reason}')
-        await ctx.send(embed=discord.Embed(title='Member Banned',
+        await ctx.send(embed=discord.Embed(title=':no_entry: Member Banned :no_entry:',
                                            description=f'{target.mention} has been banned \nReason: {reason}'))
+
+    @commands.command(name='warn')
+    @commands.has_guild_permissions(kick_members=True)
+    @moderation_check
+    async def _warn(self, ctx, target: discord.Member, *, reason: str = None):
+        try:
+            await target.send(f'You have been :warning: warned :warning: in **{ctx.guild.name}**. \nReason: {reason}')
+        except:
+            pass
+        await self.log(action='warn', moderator=ctx.author, target=target, reason=reason)
+        # warn them here
+        await ctx.send(embed=discord.Embed(title=':warning: Member Warned :warning:',
+                                           description=f'{target.mention} has been warned \nReason: {reason}'))
+    
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
