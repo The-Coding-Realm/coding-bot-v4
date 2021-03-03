86af01f36ef98b18c814480b62b60594f3acff66
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-03-02 22:45:22 -0600

    lmao dumdum

--- cogs/mod.py
+++ cogs/mod.py
@@ -10,7 +10,7 @@ class Moderation(commands.Cog):
     @commands.bot_has_guild_permissions(kick_members=True)
     @commands.has_guild_permissions(kick_members=True)
     async def _kick(self, ctx, target: discord.Member, reason: str = None):
-        if author.top_role <= target.top_role:
+        if ctx.author.top_role <= target.top_role:
             return await ctx.send(embed=ctx.error('You cannot use moderation commands on users higher or equal to you.'))
         try:
             await target.send(f'You have been kicked from **{ctx.guild.name}**. \nReason: {reason}')
