fe0d1ae947f0cd2d176da6145322879428349270
BobDotCom <71356958+BobDotCom@users.noreply.github.com>
2021-05-19 12:49:33 -0500

    Update general.py

--- cogs/general.py
+++ cogs/general.py
@@ -36,7 +36,7 @@ class RedirectMenu(menus.ListPageSource):
 
 
 def convert_link(content):
-    if re.match(r'^http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$', content):
+    if re.match(r'^[http[s]]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*(),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$', content):
         return content
     else:
         raise ValueError('Not a link')
