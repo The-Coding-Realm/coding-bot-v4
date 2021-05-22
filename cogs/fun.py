107062d77269d05d9a75b823d10f77ad04be0f3e
BobDotCom <bobdotcomgt@gmail.com>
2021-05-22 00:32:19 -0500

    strip newline in define command

--- cogs/fun.py
+++ cogs/fun.py
@@ -50,7 +50,7 @@ class LyricsMenu(BaseMenu):
 
 class DefinitionMenu(BaseMenu):
     def __init__(self, data, ctx):
-        super().__init__(data.definition, ctx)
+        super().__init__(data.definition.strip('\n'), ctx)
         self.embed.title = data.word.title()
 
 
