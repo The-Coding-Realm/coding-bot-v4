be888763e64cfe2f473d7c8af4415e63d4949d78
Dorukyum <53639936+Dorukyum@users.noreply.github.com>
2021-07-18 16:34:12 +0300

    Implement message edit and delete events

--- cogs/counting.py
+++ cogs/counting.py
@@ -13,10 +13,10 @@ class Counting(commands.Cog):
         self.channel = self.bot.get_channel(
             757193068431671327  # The counting channel in TCA
         )
-        last = self.channel.last_message or await self.channel.fetch_message(
+        self.last = self.channel.last_message or await self.channel.fetch_message(
             self.channel.last_message_id
         )
-        self.current, self.last_author = int(last.content), last.author
+        self.current = int(self.last.content)
 
     @commands.Cog.listener()
     async def on_message(self, message: Message) -> None:
@@ -25,10 +25,26 @@ class Counting(commands.Cog):
         if (
             not message.content.isdigit()
             or int(message.content) != self.current + 1
-            or message.author == self.last_author
+            or message.author == self.last.author
         ):
             return await message.delete()
+        self.last = message
         self.current += 1
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before: Message, after: Message) -> None:
+        if before == self.last:
+            if (not after.content.isdigit()) or int(after.content) != self.current:
+                await after.delete()
+        else:
+            await after.delete()
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message: Message) -> None:
+        if message == self.last:
+            self.last = await message.channel.send(
+                f"{self.current} (message from {message.author.name} was deleted)"
+            )
 
 
 def setup(bot: commands.Bot) -> None:
